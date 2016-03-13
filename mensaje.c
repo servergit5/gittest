cadf6376829adf0e0feefa696e4cecec719ffcdd
servergit5 <d12_dany@hotmail.com>
2016-03-13 01:57:06 -0300

    estoy subiendo esto en c mensaje.c

---
+++ mensaje.c
@@ -0,0 +1,10 @@
+/*
+* mensaje.c - Implementacion de la funcion imprimir_mensaje()
+*/
+#include <stdio.h>
+#include "mensaje.h"
+
+void imprimir_mensaje(char *mensaje)
+{
+	printf("%s\n", mensaje);
+}
