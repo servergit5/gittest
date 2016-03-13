b02295d250c3760aa46fc39d49ce133a0bb1351e
servergit5 <d12_dany@hotmail.com>
2016-03-13 01:41:38 -0300

    estoy subiendo este codigo Ejemplo.java

---
+++ Ejemplo.java
@@ -0,0 +1,43 @@
+
+/**
+ * Write a description of class Ejemplo here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Ejemplo
+{
+   public static void main(String args[] ){
+	Ejemplo e=new Ejemplo();
+	int num1=13;
+	int num2=24;	
+	boolean aux1=e.esPrimo(num1);
+	boolean aux2=e.esPrimo(num2);
+	
+	System.out.println(num1+" es primo?: "+aux1);
+	System.out.println(num2+" es primo?: "+aux2);
+	}
+   public boolean esPrimo(int numero)
+   {
+       return esPrimo(numero,2);
+   }
+   
+   private boolean esPrimo(int numero, int divisor)
+   {
+       boolean respuesta= true;
+       if(divisor<numero)
+       {
+          if(numero%divisor==0)
+          {
+              respuesta=false;
+          }
+          else
+          {
+              respuesta= respuesta&&esPrimo(numero,divisor+1);
+          }
+       }
+       if(numero==1)
+            respuesta=false;
+       return respuesta;
+   }
+}
